b905d9de35b0e5e0139d63483048611d0431b8c3
ashrafuddin1496 <asraf1496@gmail.com>
2020-12-27 10:31:35 +0600

    table migragtion done

---
+++ database/migrations/2020_12_27_042357_create_admins_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('username')->unique();
+            $table->string('email')->unique();
+            $table->string('cell') ->nullable();
+            $table->string('password');
+            $table->unsignedBigInteger('role')->default(2);
+            $table->string('photo')->default('avatar.jpg');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+}
